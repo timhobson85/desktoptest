5581d272cb0825949eed3d22c42352f74c8a55e2
timhobson85 <claehobson@gmail.com>
2020-04-03 14:22:39 +1100

    test3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello2
+          Hello2222
         </p>
       </header>
     </div>
