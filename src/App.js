80e5e0aa978e153f2715bccac1578eb0f6742dc5
timhobson85 <claehobson@gmail.com>
2020-04-03 14:52:59 +1100

    testing login

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello2222
+          Hello2x222
         </p>
       </header>
     </div>
